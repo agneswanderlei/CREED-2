0db31cbfd8c3c7eeedb81bd08710ef17d11232b5
Agnes Wanderlei <agneswanderlei@gmail.com>
2025-05-14 19:25:37 -0300

    coloquei o static para nao usar os dados da internet.

--- app/settings.py
+++ app/settings.py
@@ -115,9 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app', 'templates', 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Onde os arquivos serão coletados
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
